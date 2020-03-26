b5dd320b949e5e6c40e178a33a0b209b3521948a
Rob Scott <rdavidscott1@gmail.com>
2020-03-26 12:56:32 -0700

    recommit completedList

--- todolist/src/CompletedList.js
+++ todolist/src/CompletedList.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 
 class CompletedList extends Component {
-    state = {  }
+    constructor(props) {
+        super(props);
+    
+        state = { 
+            items: []
+        };
+
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+    }
+
+    addItem(e){}
+
+
     render() { 
         return (  );
     }
