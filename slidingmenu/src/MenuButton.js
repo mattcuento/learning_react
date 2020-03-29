ad5fc27e21de8fb857e738d7cbbce8c47ed1579f
Matt Cuento <cuentomr17@gmail.com>
2020-03-29 15:33:53 -0700

    sliding menu complete and added purecomponent

--- slidingmenu/src/MenuButton.js
+++ slidingmenu/src/MenuButton.js
@@ -1,7 +1,16 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import "./MenuButton.css";
 
-class MenuButton extends Component {
+class MenuButton extends PureComponent {
+//class MenuButton extends Component {
+    /*shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.handleMouseDown == this.props.handleMouseDown) {
+            return false;
+        } else {
+            return true;
+        }
+    }*/
+
     render() {
         return (
             <button id="roundButton" onMouseDown={this.props.handleMouseDown}></button>
